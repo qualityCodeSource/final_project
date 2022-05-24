05e842ba26563d28ce9f22cc10ee5ac477b5700b
mymac <connormarks14@gmail.com>
2022-05-24 15:37:13 -0700

    added International and Disabled Student Class

--- src/main/java/com/code_source/final_project/model/DisabledStudent.java
+++ src/main/java/com/code_source/final_project/model/DisabledStudent.java
@@ -5,10 +5,12 @@ import java.util.Objects;
 public class DisabledStudent extends Student{
 
     String mDisability;
+    boolean mAssistanceNeeded;
 
-    public DisabledStudent(String name, String DOB, String gender, String languages, String education, String location, String disability) {
+    public DisabledStudent(String name, String DOB, String gender, String languages, String education, String location, String disability, boolean assistanceNeeded) {
         super(name, DOB, gender, languages, education, location);
         mDisability = disability;
+        mAssistanceNeeded = assistanceNeeded;
     }
 
     public String getDisability() {
@@ -19,24 +21,33 @@ public class DisabledStudent extends Student{
         mDisability = disability;
     }
 
+    public boolean isAssistanceNeeded() {
+        return mAssistanceNeeded;
+    }
+
+    public void setAssistanceNeeded(boolean assistanceNeeded) {
+        mAssistanceNeeded = assistanceNeeded;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         DisabledStudent that = (DisabledStudent) o;
-        return Objects.equals(mDisability, that.mDisability);
+        return mAssistanceNeeded == that.mAssistanceNeeded && Objects.equals(mDisability, that.mDisability);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), mDisability);
+        return Objects.hash(super.hashCode(), mDisability, mAssistanceNeeded);
     }
 
     @Override
     public String toString() {
         return "DisabledStudent{" +
-                "mDisability='" + mDisability + '\'' +
+                "Disability='" + mDisability + '\'' +
+                ", AssistanceNeeded=" + mAssistanceNeeded +
                 '}';
     }
 }
--- src/main/java/com/code_source/final_project/model/InternationalStudent.java
+++ src/main/java/com/code_source/final_project/model/InternationalStudent.java
@@ -5,10 +5,12 @@ import java.util.Objects;
 public class InternationalStudent extends Student{
 
     String mCountryOfOrigin;
+    boolean mHostFamily;
 
-    public InternationalStudent(String name, String DOB, String gender, String languages, String education, String location, String countryOfOrigin) {
+    public InternationalStudent(String name, String DOB, String gender, String languages, String education, String location, String countryOfOrigin, boolean hostFamily) {
         super(name, DOB, gender, languages, education, location);
         mCountryOfOrigin = countryOfOrigin;
+        mHostFamily = hostFamily;
     }
 
     public String getCountryOfOrigin() {
@@ -19,26 +21,33 @@ public class InternationalStudent extends Student{
         mCountryOfOrigin = countryOfOrigin;
     }
 
+    public boolean isHostFamily() {
+        return mHostFamily;
+    }
+
+    public void setHostFamily(boolean hostFamily) {
+        mHostFamily = hostFamily;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         InternationalStudent that = (InternationalStudent) o;
-        return Objects.equals(mCountryOfOrigin, that.mCountryOfOrigin);
+        return mHostFamily == that.mHostFamily && Objects.equals(mCountryOfOrigin, that.mCountryOfOrigin);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), mCountryOfOrigin);
+        return Objects.hash(super.hashCode(), mCountryOfOrigin, mHostFamily);
     }
 
     @Override
     public String toString() {
         return "InternationalStudent{" +
                 "CountryOfOrigin='" + mCountryOfOrigin + '\'' +
+                ", HostFamily=" + mHostFamily +
                 '}';
     }
-
-
 }
